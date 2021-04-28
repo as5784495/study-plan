e4af5f30a1c50e5f3de59ba90b2ecc1be4ae1cea
amaza05693 <zzssddff5784495@gmail.com>
2021-04-28 20:40:14 +0800

    doPost練習

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 enum Func {
     Schedule = "schedule",  
     Subject = "subject", 
-    Comment = "Comment",
+    Comment = "comment",
     Tag = "tag",
     Plan = "plan", 
     RestTimeRecod = "restTimeRecod",
@@ -14,10 +14,16 @@ type DoGet = GoogleAppsScript.Events.DoGet & {
     },
 };
 
+type DoPost = GoogleAppsScript.Events.DoPost & {
+    parameter: {
+        func: Func
+    }
+}
+
 function doGet(e: DoGet) {
     const  {func}  = e.parameter
     let result: string;
-    let multi: GoogleAppsScript.Spreadsheet.Range[][]
+    let multi: any[][];
     
     for (const v in e.parameter) {
         if(v!="func")
@@ -32,7 +38,7 @@ function doGet(e: DoGet) {
     switch (func){
         case Func.Schedule:
             result = "This is Schedule."
-            multi = sheet.getRange("B2:F11").getValues();
+            multi = sheet.getRange("A2:G11").getValues();
             console.log(multi);
             break;
         case Func.Plan:
@@ -60,8 +66,58 @@ function doGet(e: DoGet) {
             // throw new Error("沒有這個功能");
             break;
     }
-    SpreadsheetApp.flush();
     return ContentService.createTextOutput(JSON.stringify(multi)).setMimeType(ContentService.MimeType.TEXT);
     
     // return ContentService.createTextOutput(JSON.stringify(e)).setMimeType(ContentService.MimeType.JSON);
+}
+
+function doPost(e: DoPost){
+    const {func} = e.parameter;
+    let result: string;
+    let multi:any[][];
+
+    for (const v in e.parameter) {
+        if(v!="func")
+            {
+                console.error("沒有這個功能");
+                return ContentService.createTextOutput("沒有這個功能").setMimeType(ContentService.MimeType.TEXT);
+            }
+    }
+
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getSheetByName(`${ func }`);
+
+    switch (func){
+        case Func.Schedule:
+            result = "This is Schedule."
+            break;
+        case Func.Plan:
+            result = "This is Plan."
+            break;
+        case Func.Comment:
+            result = "This is Comment."
+            multi = [[123,"延期"]];
+            sheet.getRange("A2:B2").setValues(multi);
+            break;
+        case Func.Tag:
+            result = "This is Tag."
+            break;
+        case Func.Subject:
+            result = "This is Subject."
+            break;
+        case Func.ScheduleMonitor:
+            result = "This is ScheduleMonitor."
+            break;
+        case Func.RestTimeRecod:
+            result = "This is RestTimeRecod."
+            break;
+            
+        default:
+            console.error("沒有這個功能");
+            return ContentService.createTextOutput("沒有這個功能").setMimeType(ContentService.MimeType.TEXT);
+            // throw new Error("沒有這個功能");
+    }
+    return ContentService.createTextOutput(result).setMimeType(ContentService.MimeType.TEXT);
+
+
 }
